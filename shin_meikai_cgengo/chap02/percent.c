554112cc8e45a22ff9479243f0b848a2f92ef1db
Nishikawa1208 <2023jouchan1117@gmail.com>
2025-04-27 15:56:56 +0900

    bohyoh.comから引っ張って来たソースコードに置き換え

--- shin_meikai_cgengo/chap02/percent.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    puts("%");
-    printf("%%\n");
-    
-    return 0;
-}
--- shin_meikai_cgengo/chap09/ensyuu0901.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-    char str[] = "ABC\0DEF";
-
-    printf("文字列strは\"%s\"です。\n", str);
-
-    return 0;
-}
